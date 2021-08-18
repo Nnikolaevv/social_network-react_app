bc1974474a088bd0d0b41cea9de698dcd7d8e658
NNikolaevv <nexon091@gmail.com>
2021-08-18 18:40:13 +0300

    'dialogsAdd'

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 
 const App = () => {
@@ -9,7 +10,10 @@ const App = () => {
     <div className={"app-wrapper"}>
       <Header />
       <Navbar />
-      <Profile />
+      <div className={'app-wrapper-content'} >
+      {/*<Profile />*/}
+      <Dialogs />
+      </div>
     </div>
   );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
   return (
-    <div className={s.content}>
+    <div>
       <div>
         <img src="https://via.placeholder.com/1000x300"/>
       </div>
